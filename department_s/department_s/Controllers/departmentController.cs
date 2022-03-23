443629ee3e644475493ad5c4ea8829f3870dac0a
xcj7 <xcjtushar@gmail.com>
2022-03-23 10:44:33 +0600

    department and student apies

---
+++ department_s/department_s/Controllers/departmentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using department_s.Models.Database;
+
+namespace department_s.Controllers
+{
+    public class departmentController : ApiController
+    {
+        department_student_apiEntities db = new department_student_apiEntities();
+        [HttpPost]
+        public void Post(department value)
+        {
+
+
+
+            db.departments.Add(value);
+            db.SaveChanges();
+
+        }
+
+
+        public department Get(int id)
+        {
+            //  data.students.
+            return db.departments.Where(x => x.dep_id == id).FirstOrDefault();
+
+        }
+
+
+        [HttpDelete]
+        // DELETE: api/student/5
+        public HttpResponseMessage Delete(int id)
+        {
+            var department = db.departments.Where(l => l.dep_id.Equals(id)).FirstOrDefault();
+            db.departments.Remove(department);
+            db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, department);
+        }
+
+
+    }
+}
---
+++ department_s/department_s/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="department_s.WebApiApplication" Language="C#" %>
